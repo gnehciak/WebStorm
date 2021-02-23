47bffa8b782e22d924f30c5ed450bcd029e9e03a
gnehciak <lkc364636722@gmail.com>
2021-02-23 23:53:21 +1100

    ...

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let hasV, hasA, hasU, hasT;
-hasV = hasU = hasA = hasT = false
-let valV, valA, valU, valT;
+let hasV, hasA, hasU, hasT, hasS;
+hasV = hasU = hasA = hasT = hasS = false
+let valV, valA, valU, valT, valS;
 let missingKey = '';
 /*
 function calculate1() {
@@ -33,7 +33,12 @@ function checkIPT(v) {
     }
 
     checkEq1();
-
+    checkEq2();
+    if(document.getElementById("ipt_V").value != '') document.getElementById("calc_V").value = '';
+    if(document.getElementById("ipt_A").value != '') document.getElementById("calc_A").value = '';
+    if(document.getElementById("ipt_U").value != '') document.getElementById("calc_U").value = '';
+    if(document.getElementById("ipt_T").value != '') document.getElementById("calc_T").value = '';
+    if(document.getElementById("ipt_S").value != '') document.getElementById("calc_S").value = '';
 }
 
 function checkEq1() {
@@ -41,12 +46,12 @@ function checkEq1() {
     const arr = [hasV, hasU, hasA, hasT];
     const arrdict = {'V':hasV, 'U':hasU, 'A':hasA, 'T':hasT};
     const count = arr.filter(Boolean).length;
-    console.log("Amount of true is: " + count)
+    //console.log(`Amount of true is: ${count}`)
     if(arr.length - count == 1){
-        console.log("equation 1 is available.")
+        //console.log("equation 1 is available.")
         for(let key in arrdict){
             if(arrdict[key] == false){
-                console.log("Missing variable is: " + key);
+                //console.log("Missing variable is: " + key);
                 missingKey = key
             }
         }
@@ -69,17 +74,44 @@ function checkEq1() {
                 break;
         }
     }
-    else{
-        document.getElementById("calc_V").value = '';
-        document.getElementById("calc_U").value = '';
-        document.getElementById("calc_A").value = '';
-        document.getElementById("calc_T").value = '';
+}
+function checkEq2() {
+
+    const arr = [hasV, hasU, hasT, hasS];
+    const arrdict = {'V': hasV, 'U': hasU, 'T': hasT, 'S': hasS};
+    const count = arr.filter(Boolean).length;
+    console.log(`Amount of true is: ${count}`)
+    if (arr.length - count == 1) {
+        console.log("equation 2 is available.")
+        for (let key in arrdict) {
+            if (arrdict[key] == false) {
+                console.log("Missing variable is: " + key);
+                missingKey = key
+            }
+        }
+        switch (missingKey){
+            case "":
+                break;
+            case 'V':
+                document.getElementById("calc_V").value = (2 * valS) / valT - valU;
+                break;
+            case 'U':
+                document.getElementById("calc_U").value = (2 * valS) / valT - valV;
+                break;
+            case 'T':
+                document.getElementById("calc_T").value = (2 * valS) / (valU + valV) ;
+                break;
+            case 'S':
+                document.getElementById("calc_S").value = ((valU + valV) / 2) * valT;
+                break;
+            default:
+                break;
+        }
     }
 }
-
 /*
 * v	= u + at
-* s = (u+v)/s * t
+* s = (u+v)/2 * t
 * v^2 = u^2 + 2as
 * s = ut + 1/2(at^2)
 * s = vt - 1/2(at^2)
